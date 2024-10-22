83efb1c32d861b64578eb33a154e2bb51ea2a60a
unknown <romedzmedel2003@gmail.com>
2024-10-22 17:41:51 +0800

    test

--- resources/views/livewire/new-user.blade.php
+++ resources/views/livewire/new-user.blade.php
@@ -23,7 +23,7 @@
        
 
         <div class="mt-6 w-1/2">
-            <input wire:model.live="password" type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="•••••••••" required />
+            <input wire:model.live="password" type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Password" required />
         </div>
             @error('password')
               <span class="text-red-500 text-sm">{{ $message }}</span>
